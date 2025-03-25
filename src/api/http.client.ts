4fb65b5049b62c24933a4999a9af696987b58a55
Ng Ping <ping.ng_gds@scientecpeople.com>
2025-03-25 19:01:02 +0800

    fix: refactor base url handling

--- src/api/http.client.ts
+++ src/api/http.client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-console.log("process.env.BASE_URL", process.env.BASE_URL);
+console.log("BASE_URL", process.env.BASE_URL);
+
 export const api = axios.create({ baseURL: process.env.BASE_URL });
